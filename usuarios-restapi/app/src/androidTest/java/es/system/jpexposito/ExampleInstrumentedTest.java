d4cfd68d3a398d5a3981cf547c4906c0c42689db
Joatham Pérez Expósito <jpe.gsc@gmail.com>
2022-01-21 17:48:11 +0000

    Se actualiza la url del servicio y el activity principal

--- usuarios-restapi/app/src/androidTest/java/es/system/jpexposito/ExampleInstrumentedTest.java
+++ usuarios-restapi/app/src/androidTest/java/es/system/jpexposito/ExampleInstrumentedTest.java
@@ -5,22 +5,35 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import es.system.jpexposito.model.User;
+import es.system.jpexposito.networking.connection.APIUtils;
+import es.system.jpexposito.networking.connection.UserService;
+import retrofit2.Call;
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    UserService userService;
+
+    @Before
+    public void before(){
+        if (userService == null) {
+            userService = APIUtils.getUserService();
+        }
+    }
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("es.system.jpexposito", appContext.getPackageName());
     }
 }
--- usuarios-restapi/app/src/main/java/es/system/jpexposito/networking/connection/APIUtils.java
+++ usuarios-restapi/app/src/main/java/es/system/jpexposito/networking/connection/APIUtils.java
@@ -7,7 +7,7 @@ public class APIUtils {
     private APIUtils(){
     };
 
-    public static final String API_URL = "url del servicio";
+    public static final String API_URL = "http://localhost:8080/api/v1/";
 
     public static UserService getUserService(){
         return RetrofitClient.getClient(API_URL).create(UserService.class);
